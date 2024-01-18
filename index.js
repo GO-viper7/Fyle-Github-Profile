7b7ae140a9a9136bc28754107cd0b06455f184b8
GoViper <nithishbanda2021@gmail.com>
2024-01-19 03:26:50 +0530

    bug

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-let accessToken = ""
-fetch('/api/getApiKey')
-  .then(response => response.json())
-  .then(data => {
-      
-  accessToken = data.apiKey;
-  console.log(accessToken)
-  const apiUrl = "https://api.github.com/user";
-  fetch(apiUrl, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("profile-name").innerText = data.name || data.login;
-      document.getElementById("profile-description").innerText =
-        data.bio || "No description available";
-      document.getElementById("profile-location").innerText =
-        data.location || "Location not specified";
-      document.getElementById("github-link").href = data.html_url;
+fetch("/api/getApiKey")
+  .then((response) => response.json())
+  .then((data) => {
+    const accessToken = data.apiKey;
+    console.log(accessToken);
+    const apiUrl = "https://api.github.com/user";
+    fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("profile-name").innerText =
+          data.name || data.login;
+        document.getElementById("profile-description").innerText =
+          data.bio || "No description available";
+        document.getElementById("profile-location").innerText =
+          data.location || "Location not specified";
+        document.getElementById("github-link").href = data.html_url;
 
-      document.getElementById("profile-pic").src = data.avatar_url;
+        document.getElementById("profile-pic").src = data.avatar_url;
 
-      const reposUrl = `${apiUrl}/repos?per_page=${reposPerPage}&page=${currentPage}`;
-      fetch(reposUrl, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((repos) => {
-          const repoContainer = document.getElementById("repo-container");
-          repoContainer.innerHTML = "";
-          console.log(repos)
-          repos.forEach((repo) => {
-            const repoCard = document.createElement("div");
-            repoCard.className = "card col-md-4 repo-card mr-2";
-            repoCard.innerHTML = `
+        const reposUrl = `${apiUrl}/repos?per_page=${reposPerPage}&page=${currentPage}`;
+        fetch(reposUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((repos) => {
+            const repoContainer = document.getElementById("repo-container");
+            repoContainer.innerHTML = "";
+            console.log(repos);
+            repos.forEach((repo) => {
+              const repoCard = document.createElement("div");
+              repoCard.className = "card col-md-4 repo-card mr-2";
+              repoCard.innerHTML = `
                   <div class="card-body" >
                     <h5 class="card-title">${repo.name}</h5>
                     <p class="card-text">${
@@ -54,32 +53,31 @@ fetch('/api/getApiKey')
                     }</p>
                   </div>
                 `;
-            repoContainer.appendChild(repoCard);
-          });
-        })
-        .catch((error) => console.error("Error fetching repositories:", error));
-    })
-    .catch((error) =>
-      console.error("Error fetching profile information:", error)
-    );
+              repoContainer.appendChild(repoCard);
+            });
+          })
+          .catch((error) =>
+            console.error("Error fetching repositories:", error)
+          );
+      })
+      .catch((error) =>
+        console.error("Error fetching profile information:", error)
+      );
 
-})
-.catch(error => console.error('Error fetching API key:', error));
+    let reposPerPage = 10;
+    let currentPage = 1;
 
-let reposPerPage = 10;
-let currentPage = 1;
+    function displayRepositories(repos) {
+      const repoContainer = document.getElementById("repo-container");
+      repoContainer.innerHTML = "";
 
-function displayRepositories(repos) {
-  const repoContainer = document.getElementById("repo-container");
-  repoContainer.innerHTML = "";
+      const startIndex = (currentPage - 1) * reposPerPage;
+      const endIndex = startIndex + reposPerPage;
 
-  const startIndex = (currentPage - 1) * reposPerPage;
-  const endIndex = startIndex + reposPerPage;
-
-  repos.forEach((repo) => {
-    const repoCard = document.createElement("div");
-    repoCard.className = "card col-md-4 repo-card mr-2";
-    repoCard.innerHTML = `
+      repos.forEach((repo) => {
+        const repoCard = document.createElement("div");
+        repoCard.className = "card col-md-4 repo-card mr-2";
+        repoCard.innerHTML = `
           <div class="card-body" >
             <h5 class="card-title">${repo.name}</h5>
             <p class="card-text">${
@@ -97,84 +95,90 @@ function displayRepositories(repos) {
             }</p>
           </div>
         `;
-    repoContainer.appendChild(repoCard);
-  });
+        repoContainer.appendChild(repoCard);
+      });
 
-  updatePagination();
-}
-document.getElementById('number-badge').innerHTML = 10
-function setReposPerPage(count) {
-  document.getElementById('number-badge').innerHTML = count
-  reposPerPage = count;
-  currentPage = 1;
-  fetchDataAndDisplay();
-}
-
-function updatePagination() {
-  const paginationList = document.querySelectorAll(".pagination .page-item");
-
-  paginationList.forEach((item) => {
-    if (item.querySelector(".page-link").innerText == currentPage) {
-      item.classList.add("active");
-    } else {
-      item.classList.remove("active");
+      updatePagination();
     }
-  });
-}
+    document.getElementById("number-badge").innerHTML = 10;
+    function setReposPerPage(count) {
+      document.getElementById("number-badge").innerHTML = count;
+      reposPerPage = count;
+      currentPage = 1;
+      fetchDataAndDisplay();
+    }
 
+    function updatePagination() {
+      const paginationList = document.querySelectorAll(
+        ".pagination .page-item"
+      );
 
-function fetchDataAndDisplay() {
-  const url = `${apiUrl}/repos?per_page=${reposPerPage}&page=${currentPage}`;
-  fetch(url, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-    .then((response) => response.json())
-    .then((repos) => {
-      console.log(repos);
-      displayRepositories(repos);
-    })
-    .catch((error) => console.error("Error fetching repositories:", error));
-}
+      paginationList.forEach((item) => {
+        if (item.querySelector(".page-link").innerText == currentPage) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    }
 
-document.getElementById("prev-page").addEventListener("click", () => {
-  if (currentPage > 1) {
-    currentPage--;
-    fetchDataAndDisplay();
-  }
-});
+    function fetchDataAndDisplay() {
+      const url = `${apiUrl}/repos?per_page=${reposPerPage}&page=${currentPage}`;
+      fetch(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((repos) => {
+          console.log(repos);
+          displayRepositories(repos);
+        })
+        .catch((error) => console.error("Error fetching repositories:", error));
+    }
 
-document.getElementById("next-page").addEventListener("click", () => {
-  if (currentPage < 9) {
-    currentPage++;
-    fetchDataAndDisplay();
-  }
-});
-
-for (let i = 1; i <= 9; i++) {
-  document.getElementById(`page-${i}`).addEventListener(
-    "click",
-    ((page) => {
-      return () => {
-        console.log(`Clicked on page ${page}`);
-        currentPage = page;
+    document.getElementById("prev-page").addEventListener("click", () => {
+      if (currentPage > 1) {
+        currentPage--;
         fetchDataAndDisplay();
-      };
-    })(i)
-  );
-}
-const backToTopBtn = document.getElementById("backToTopBtn");
+      }
+    });
 
-window.addEventListener("scroll", () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTopBtn.style.display = "block";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
-});
+    document.getElementById("next-page").addEventListener("click", () => {
+      if (currentPage < 9) {
+        currentPage++;
+        fetchDataAndDisplay();
+      }
+    });
 
-backToTopBtn.addEventListener("click", () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
+    for (let i = 1; i <= 9; i++) {
+      document.getElementById(`page-${i}`).addEventListener(
+        "click",
+        ((page) => {
+          return () => {
+            console.log(`Clicked on page ${page}`);
+            currentPage = page;
+            fetchDataAndDisplay();
+          };
+        })(i)
+      );
+    }
+    const backToTopBtn = document.getElementById("backToTopBtn");
+
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        backToTopBtn.style.display = "block";
+      } else {
+        backToTopBtn.style.display = "none";
+      }
+    });
+
+    backToTopBtn.addEventListener("click", () => {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    });
+  })
+  .catch((error) => console.error("Error fetching API key:", error));
